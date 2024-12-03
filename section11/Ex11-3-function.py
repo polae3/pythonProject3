73add825b571008d8b739ade725919f61efca058
polae3 <polae3@gmail.com>
2024-12-03 21:08:16 +0900

    fdgfg

---
+++ section11/Ex11-3-function.py
@@ -0,0 +1,14 @@
+'''
+파일명: Ex11-2-function.py
+
+Return
+    함수는 작업을 수행항 결과를 반환(return)할 수 있다.
+    반환된 값은 함수 호출한 위치에서 사용할 수 있다.
+'''
+
+def address():
+    str = '''우편번호 12345
+    서울시 영등포구 여의도동'''
+    return str
+
+result = address()
